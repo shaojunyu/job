664109e6af932ca9268338f0e59402568b426bf3
ASUS-YUSHAOJUN\96853 <968538@163.com>
2016-10-19 01:25:42 +0800

    no message

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -62,7 +62,7 @@ class User extends CI_Controller{
         $session_data = $this->session->userdata();
         if (empty($session_data['tag1']) or empty($session_data['tag2'])){
             header('Location:./complete_info/1');
-        }else if (empty($session_data['name']) or empty($session_data['sex']) or empty($session_data[])){
+        }else if (empty($session_data['name']) or empty($session_data['sex']) or empty($session_data['QQ'])){
 
         }
 //        ?\$this->load->view('myself_view');
