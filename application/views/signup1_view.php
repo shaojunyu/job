8c383d23f16738eaca6ea3ce26e6958e3bdd732e
hanrenguang <413526182@qq.com>
2016-10-18 23:45:54 +0800

    no message

---
+++ application/views/signup1_view.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<title>注册</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/signup1.css">
+</head>
+<body>
+	<div class="logo">
+		<img src="images/logo.png">
+		<p>四年兼职注册</p>
+	</div>
+
+	<form>
+		<input type="tel" class="phone" placeholder="请输入手机号">
+
+		<div class="codes-box clearfix">
+			<input type="tel" class="codes" placeholder="输入验证码">
+			<a href="javascript:;" class="get">获取验证码</a>
+		</div>
+		
+		<a href="javascript:;" class="next">下一步</a>
+	</form>
+
+	<script type="text/javascript" src="js/zepto.min.js"></script>
+	<script type="text/javascript" src="js/touch.js"></script>
+	<script type="text/javascript" src="js/signup1.js"></script>
+</body>
+</html>
