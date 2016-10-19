0917d8828b592f91873e24520a3e7fa4034f0a54
hanrenguang <413526182@qq.com>
2016-10-19 11:07:42 +0800

    no message

--- js/signup2.js
+++ js/signup2.js
@@ -1,6 +1,4 @@
 $(function () {
-	
-
 	//选择学校
 	var $school = $(".school");
 	var $allSchool = $school.find("a");
@@ -24,9 +22,52 @@ $(function () {
 		$school.hide();
 	});
 
+	//密码输入处理
+	$(".password").focus(function() { //获得焦点
+		$(this).attr("type", "password");
+		if($(this).css("color") == "red") {
+			hideError($(this));
+		}
+	});
+
 	//注册
 	$(".next").tap(function () {
+		//验证学校
+		if($(".choose").html() === "请选择您所在的学校") {
+			showMsg("请选择您所在的学校");
+			return;
+		}
+		//验证密码
+		var isPasswordOk = formMathod.passwdCheck($(".password")[0]);
+		if(typeof isPasswordOk === "string") {
+			$(".password").attr("type", "text");
+			showError($(".password"), isPasswordOk);
+			return;
+		}
 
+		var data = {
+			cellphone: $(".phone").val().split("-").join(""),
+			school: $(".choose").html(),
+			password: $(".password").val()
+		};
+		$.ajax({
+	        url: '../api/signup',
+	        contentType: "application/json",
+	        dataType: "json",
+	        type: "POST",
+	        data: JSON.stringify(data),
+	        success: function(data) {
+	        	if(data.success) {
+	        		window.location.href = "../user/complete_info";
+	        	}
+				else {
+	        		showMsg(data.msg);
+	        	}
+	        },
+		    error: function(XMLHttpRequest, textStatus, errorThrown){  
+		        showMsg("请求失败，请刷新重试"); 
+		    }
+	    });
 	});
 });
 
@@ -79,3 +120,30 @@ formMathod.passwdCheck = function(input) { //验证非中文和全角
 		return msg;
 	}
 };
+
+//显示错误提示
+function showError(inputElem, err) {
+	inputElem.css("color", "red");
+	inputElem.val(err);
+}
+
+//隐藏错误提示
+function hideError(inputElem) {
+	inputElem.css("color", "#000");
+	inputElem.val("");
+}
+
+//消息显示框显示
+function showMsg(msg) {
+	$(".prompt-box").html(msg);
+	$(".prompt-box").show();
+	setTimeout(function () {
+		hideMsg();
+	}, 3000);
+}
+
+//消息显示框隐藏
+function hideMsg() {
+	$(".prompt-box").html("");
+	$(".prompt-box").hide();
+}
