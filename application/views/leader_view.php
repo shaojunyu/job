b970b8072e257f5310421142d06ca984b6bb3548
hanrenguang <413526182@qq.com>
2016-10-20 22:42:03 +0800

    no message

--- application/views/leader_view.php
+++ application/views/leader_view.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<title>领队管理</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/reset.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/leader-management.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/leader.css">
 </head>
 <body>
 	<div class="content">
