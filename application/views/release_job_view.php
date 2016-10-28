5f503fe9bd41579e461d6537461e9b28db8eda8f
hanrenguang <413526182@qq.com>
2016-10-28 23:02:05 +0800

    no message

---
+++ application/views/release_job_view.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<title>发布兼职</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/reset.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/release-job.css">
+</head>
+<body>
+	<div class="content">
+		<div class="job">
+			<p class="title">四年生活线下试卷销售</p>
+			<p class="detailed">发布时间：<span class="time">2016.09.19 23:39</span>&nbsp;&nbsp;<a href="javascript:;">查看详情 ></a></p>
+			<div class="link-box clearfix">
+				<div><a href="javascript:;">查看情况</a></div>
+				<div><a href="javascript:;">发工资</a></div>
+			</div>
+		</div>
+	</div>
+
+	<div class="release-wrapper"><a href="javascript:;" class="release">发布新的兼职</a></div>
+
+	<div class="bottom-bar clearfix">
+		<a href="javascript:;">发布兼职</a>
+		<a href="javascript:;">商户中心</a>
+	</div>
+
+	<script type="text/javascript" src="<?php echo base_url();?>js/zepto.min.js"></script>
+	<script type="text/javascript" src="<?php echo base_url();?>js/touch.js"></script>
+	<script type="text/javascript" src="<?php echo base_url();?>js/my-job.js"></script>
+</body>
+</html>
