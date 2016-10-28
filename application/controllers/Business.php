06b9a9b664bd9c563efb3a950b55769f3ee2eac5
ASUS-YUSHAOJUN\96853 <968538@163.com>
2016-10-28 22:51:20 +0800

    no message

--- application/controllers/Business.php
+++ application/controllers/Business.php
@@ -18,8 +18,23 @@ class Business extends CI_Controller{
 
     public function login()
     {
-    	$this->load->view('business_login_view');
-    	# code...
+    	$cellphone = $this->input->post('cellphone');
+    	$password = $this->input->post('password');
+    	if ( !empty($cellphone) and !empty($password) ) {
+    		$this->db->where('cellphone', $cellphone);
+    		$this->db->where('password', $password);
+    		$this->db->where('approved','YES');
+    		$res = $this->db->count_all_results('job_business');
+    		// echo $res;
+    		if ($res == 1) {
+    			$this->session->set_userdata('business','business');
+    			$this->load->view('business_view');
+    		}else{
+    			header(base_url('business/login?error'));
+    		}
+    	}else{
+    		$this->load->view('business_login_view');
+    	}
     }
 
     public function apply()
@@ -27,5 +42,5 @@ class Business extends CI_Controller{
     	# code...
     }
 
-    
+
 }
