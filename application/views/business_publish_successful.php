ac56526c428ef8d32e511ce13a24a41becbeeada
hanrenguang <413526182@qq.com>
2016-10-29 15:40:36 +0800

    no message

---
+++ application/views/business_publish_successful.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<title>发布成功</title>
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/release-successful.css">
+</head>
+<body>
+	<div class="content">
+		<p>支付并发布成功，我们将火速审核，马上开始招人</p>
+		<a href="javascript:;">返回首页</a>
+	</div>
+
+	<div class="bottom-bar">
+		<a href="javascript:;">发布兼职</a>
+		<a href="javascript:;">商户中心</a>
+	</div>
+</body>
+</html>
